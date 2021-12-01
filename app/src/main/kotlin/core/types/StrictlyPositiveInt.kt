61284b7932d4d59c6229ffa3264ef302b1479844
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-01 21:28:04 +0100

    feat(#3): add operations to strictly positive int type

--- app/src/main/kotlin/core/types/StrictlyPositiveInt.kt
+++ app/src/main/kotlin/core/types/StrictlyPositiveInt.kt
@@ -2,6 +2,12 @@
 
 package core.types
 
+infix operator fun Int.minus(other: StrictlyPositiveInt): Int =
+    this - other.value
+
+infix operator fun Int.times(other: StrictlyPositiveInt): Int =
+    this * other.value
+
 fun Int.strictlyPositive(): StrictlyPositiveInt? = StrictlyPositiveInt of this
 
 fun String.toStrictlyPositiveInt(): StrictlyPositiveInt? = toIntOrNull()
@@ -17,4 +23,13 @@ value class StrictlyPositiveInt private constructor(val value: Int) {
             value.takeIf { it > MIN }
                 ?.let(::StrictlyPositiveInt)
     }
+
+    infix operator fun minus(other: Int): Int = value - other
+
+    infix operator fun times(other: Int): Int = value * other
+
+    infix operator fun times(other: PaginationSize): Int = value * other
+
+    infix operator fun times(other: StrictlyPositiveInt): Int =
+        value * other.value
 }
--- app/src/test/kotlin/core/types/StrictlyPositiveIntTest.kt
+++ app/src/test/kotlin/core/types/StrictlyPositiveIntTest.kt
@@ -1,24 +1,67 @@
 package core.types
 
+import org.junit.jupiter.api.Nested
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class StrictlyPositiveIntTest {
-    @Test
-    fun `creation should pass`() {
-        1.strictlyPositive()
-            .let(::assertNotNull)
-        "1".toStrictlyPositiveInt()
-            .let(::assertNotNull)
+    private companion object {
+        private const val VALID_INT: Int = 1
     }
 
-    @Test
-    fun `creation should fail`() {
-        0.strictlyPositive()
-            .let(::assertNull)
-        listOf("0", "a")
-            .map(String::toStrictlyPositiveInt)
-            .forEach(::assertNull)
+    @Nested
+    inner class Creation {
+        @Test
+        fun `should pass`() {
+            VALID_INT.strictlyPositive()
+                .let(::assertNotNull)
+            "1".toStrictlyPositiveInt()
+                .let(::assertNotNull)
+        }
+
+        @Test
+        fun `should fail`() {
+            0.strictlyPositive()
+                .let(::assertNull)
+            listOf("0", "a")
+                .map(String::toStrictlyPositiveInt)
+                .forEach(::assertNull)
+        }
+    }
+
+    @Nested
+    inner class Minus {
+        @Test
+        fun `should pass with Int`() {
+            val x: StrictlyPositiveInt = VALID_INT.strictlyPositive()!!
+            val y = 1
+            assertEquals(actual = x - y, expected = y - x)
+        }
+    }
+
+    @Nested
+    inner class Times {
+        @Test
+        fun `should pass with Int`() {
+            val x: StrictlyPositiveInt = VALID_INT.strictlyPositive()!!
+            val y = 2
+            assertEquals(actual = x * y, expected = y * x)
+        }
+
+        @Test
+        fun `should pass with PaginationSize`() {
+            val x: StrictlyPositiveInt = VALID_INT.strictlyPositive()!!
+            val y: PaginationSize = 15.toPaginationSize()!!
+            assertEquals(expected = x * y, actual = y * x)
+        }
+
+        @Test
+        fun `should pass with StrictlyPositiveInt`() {
+            val x: StrictlyPositiveInt = VALID_INT.strictlyPositive()!!
+            val y: StrictlyPositiveInt = 5.strictlyPositive()!!
+            assertEquals(expected = x * y, actual = y * x)
+        }
     }
 }
