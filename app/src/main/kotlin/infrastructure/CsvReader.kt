c8bf36617afc2e4e53da2dd5195e74d671106eac
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-01 15:13:31 +0100

    refactor(#3): reset implementation of paginated translations retrieving

--- app/src/main/kotlin/infrastructure/CsvReader.kt
+++
@@ -1,8 +0,0 @@
-package infrastructure
-
-import com.github.doyaaaaaken.kotlincsv.client.CsvReader
-import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
-
-val csvReader: CsvReader by lazy {
-    csvReader { skipEmptyLine = true }
-}
