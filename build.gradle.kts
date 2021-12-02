15e7b62b8d949babf53cf85395ca62d86945db45
Loïc Lamarque <loiclamarque777@gmail.com>
2021-12-02 18:47:37 +0100

    chore(#2): set application's version to 0.2.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins { kotlin(module = "jvm") version Kotlin.VERSION apply false }
 
 allprojects {
     group = "com.ekang-api"
-    version = "0.2.1"
+    version = "0.2.2"
 
     repositories(RepositoryHandler::mavenCentral)
 
