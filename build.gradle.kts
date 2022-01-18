bd8c1602259de28ee1ec6c258011029b885a9f9c
Loïc Lamarque <loiclamarque777@gmail.com>
2022-01-18 20:15:56 +0100

    chore(#6): move source code to root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,45 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins { kotlin("jvm") version embeddedKotlinVersion apply false }
-
-allprojects {
-    group = "com.ekang-api"
-    version = "1.1.0"
-
-    repositories(RepositoryHandler::mavenCentral)
-
-    tasks {
-        withType<JavaCompile> {
-            enabled = false
-            targetCompatibility = JavaVersion.VERSION_1_8.toString()
-        }
-        withType<KotlinCompile> {
-            kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
-        }
-        withType<Test> {
-            testLogging { exceptionFormat = TestExceptionFormat.FULL }
-            useJUnitPlatform()
-        }
-    }
+plugins {
+    kotlin("jvm") version embeddedKotlinVersion
+    kotlin("plugin.serialization") version embeddedKotlinVersion
+    application
 }
 
-tasks.register<Task>(name = "printVersion") {
-    doLast { println(version) }
+group = "com.ekang-api"
+version = "1.1.0"
+
+repositories(RepositoryHandler::mavenCentral)
+
+dependencies {
+    // Kotlin
+    implementation(platform(kotlin("bom")))
+    testImplementation(kotlin("test"))
+
+    // Ktor
+    fun ktor(module: String): String = "io.ktor:ktor-$module"
+    implementation(platform("${ktor("bom")}:1.6.3"))
+    implementation(ktor("server-netty"))
+    implementation(ktor("serialization"))
+    testImplementation(ktor("server-test-host"))
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.2.10")
+
+    // CSV
+    implementation("com.github.doyaaaaaken:kotlin-csv-jvm:1.2.0")
+}
+
+application.mainClass.set("AppKt")
+
+tasks {
+    compileJava { enabled = false }
+    compileTestJava { enabled = false }
+    register("printVersion") {
+        doLast { print(version) }
+    }
+    test {
+        testLogging.exceptionFormat = TestExceptionFormat.FULL
+        useJUnitPlatform()
+    }
 }
